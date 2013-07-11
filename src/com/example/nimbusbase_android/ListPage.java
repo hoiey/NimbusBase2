adb7c41e11ed65fd923ac20ef45f1cb1994560c7
Hongyan <dream.fly.on@gmail.com>
2013-07-11 22:08:55 +0800

    the Diary show list activity
    read all the data from database. and show on another layout.

---
+++ src/com/example/nimbusbase_android/ListPage.java
@@ -0,0 +1,69 @@
+package com.example.nimbusbase_android;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class ListPage extends Activity{
+
+	DataBaseAdapter dataBaseAdapter;
+	public ListView list = null;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.diary_list);
+		
+		list = (ListView)findViewById(R.id.listView1);
+		
+//		String[] arr={" 1","2","3"};
+//		
+//		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arr);
+//		
+//		list.setAdapter(adapter);
+		
+		dataBaseAdapter = new DataBaseAdapter(this);
+		
+		SQLiteDatabase db =  dataBaseAdapter.getReadableDatabase();
+		Cursor cur = db.query(DataBaseAdapter.DB_TABLE,new String[] {DataBaseAdapter.KEY_ID,
+				DataBaseAdapter.KEY_TEXT, DataBaseAdapter.KEY_CREAT_TIME, DataBaseAdapter.KEY_TAGS,
+				DataBaseAdapter.KEY_GID,DataBaseAdapter.KEY_TIME,DataBaseAdapter.KEY_ID_NIM }, null, null, null, null, null);
+		
+		if (cur != null && cur.getCount() >= 0){
+			
+			@SuppressWarnings("deprecation")
+			ListAdapter	adapter= new SimpleCursorAdapter(this,
+					android.R.layout.simple_list_item_1,
+					cur,
+					new String[] {DataBaseAdapter.KEY_TEXT, DataBaseAdapter.KEY_CREAT_TIME },
+					new int[] { android.R.id.text1, android.R.id.text2});
+	//, DataBaseAdapter.KEY_TAGS,DataBaseAdapter.KEY_GID,DataBaseAdapter.KEY_TIME,DataBaseAdapter.KEY_ID_NIM
+			list.setAdapter(adapter);
+			}
+			
+			
+		cur.close();
+		db.close();
+		
+		
+		
+//		if (cur != null && cur.getCount() >= 0)
+//		{
+//			@SuppressWarnings("deprecation")
+//			ListAdapter adapter = new SimpleCursorAdapter(this,
+//				android.R.layout.simple_list_item_1,
+//				cur,
+//				new String[] {DataBaseAdapter.KEY_TEXT, DataBaseAdapter.KEY_CREAT_TIME },
+//				new int[] { android.R.id.text1, android.R.id.text1, });
+////, DataBaseAdapter.KEY_TAGS,DataBaseAdapter.KEY_GID,DataBaseAdapter.KEY_TIME,DataBaseAdapter.KEY_ID_NIM
+//			list.setAdapter(adapter);
+//		}
+				
+	}
+	
+}
